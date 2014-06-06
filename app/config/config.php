3f426150c33b09c16a3f5d3fcaff9dd9104eb747
sergilius <panoshop@mail.ru>
2014-06-06 12:39:53 +0300

    no message

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,15 @@
 <?php  if ( ! defined('READFILE')) exit('No direct script access allowed');
 
 // пути
-$pageControllerPath = 'controllers/page_controller.php';
-$file_controlerPath = 'controllers/file_controler.php';
-$controllerPath = 'system/controller.php';
-$corePath = 'system/view.php';
-$modelPath = 'system/model.php';
-$pageModelPath = 'models/page_model.php';
+//$pageControllerPath = 'controllers/page_controller.php';
+//$file_controlerPath = 'controllers/file_controler.php';
+$catalogControllerPath = 'app/controllers/catalog_controller.php';
+$controllerPath = 'app/system/controller.php';
+$corePath = 'app/system/view.php';
+$modelPath = 'app/system/model.php';
+$catalogModel = 'app/models/catalog_model.php';
+$commonModel = 'app/models/common_model.php';
 
-?>
+
+
+/* End of file  */
--- app/system/controller.php
+++ app/system/controller.php
@@ -2,16 +2,14 @@
 
 class controller extends view {
 
-    public static function view($file_name, $options = FALSE, $content = '') {
-//options - TRUE/FALSE - read/include file
-//        $file_path = 'app/views/' . $file_name . '.php';
-        $file_path = 'views/' . $file_name . '.php';
+    public static function view($file_name, $options = FALSE, $data = '') {     //options - TRUE/FALSE - read/include file
 
+        $file_path = 'app/views/' . $file_name . '.php';
         if (file_exists(realpath($file_path))) {
             if ($options) {
                 return self::read_file($file_path);
             } else {
-                self::includ_file($file_path, $content);
+                self::includ_file($file_path, $data);
             }
         } else {
             return self::read_file($file_path);
